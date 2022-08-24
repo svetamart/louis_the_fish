f43b3868fbad73050bde8234f9977bdd1f837a0c
Sveta Martynova <svetamart95@gmail.com>
2022-08-24 16:56:47 +0300

    added example 8 code

--- example008_/Program.cs
+++ example008_/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 22;
+int b = 15;
+int c = 33;
+int d = 39;
+int e = 33;
+int f = 23;
+int g = 40;
+int h = 2;
+int i = 31;
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+if(f > max) max = f;
+if(g > max) max = g;
+if(h > max) max = h;
+if(i > max) max = i;
+
+Console.WriteLine(max);

0cd6bf69920b9b0f1ac916974ee6c7ae1b860de8
Sveta Martynova <svetamart95@gmail.com>
2022-08-24 18:11:28 +0300

    added example 010 code

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1, 34, 8, 28, 665, 9, 8, 55, 4, 28};
+
+int n = array.Length; // это типа size, размер массива
+
+int find = 28;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1; // index++
+}
+
